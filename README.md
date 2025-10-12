b103980b49ab48769b13c8ef893e728ec3f8136b
Ravindra Solanke <ravisolanke1407@gmail.com>
2025-10-12 20:00:24 +0530

    Initial commit - Portfolio

---
+++ README.md
@@ -0,0 +1,53 @@
+# 🌐 Portfolio Website
+
+A sleek, responsive, and fully customizable **portfolio website** built using **React.js** and **Tailwind CSS**.
+
+## 🚀 Live Demo
+
+🔗 [View Portfolio Live]()
+
+## 🛠️ Tech Stack
+
+- ⚛️ React.js
+- 🎨 Tailwind CSS
+- 💾 Vercel(for deployment)
+
+## 📁 Features
+
+- Responsive design for all screen sizes 📱💻
+- Smooth scroll and animation effects ✨
+- Clean and modular React components 🧩
+- Easy to customize with Tailwind utility classes 🎯
+- Professional sections: About, Skills, Projects, Contact, etc. 👨‍💻
+
+## 🧑‍💻 How to Use
+
+### 1. Clone the Repo
+
+```bash
+git clone []
+cd your-repo-name
+```
+
+### 2. Install Dependencies
+
+```bash
+npm install
+```
+
+### 3. Run the App
+
+```bash
+npm start
+```
+
+### 4. Build for Production
+
+```bash
+npm run build
+```
+
+## 🌟 Credits
+
+This project was created with ❤️ by [Ravindra Solanke]().
+If you like it, feel free to ⭐ this repository and share!
---
+++ src/constants.js
@@ -0,0 +1,274 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import chakrauiLogo from './assets/tech_logo/chakra.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import bundlerLogo from './assets/tech_logo/bundler.png';
+import eslintPrettierLogo from './assets/tech_logo/eslintPrettier.jpg';
+import jiraLogo from './assets/tech_logo/jira.png';
+import dockerLogo from './assets/tech_logo/docker.png';
+import nginxLogo from './assets/tech_logo/nginx.png';
+import nestjsLogo from './assets/tech_logo/nestjs.png';
+import jenkinsLogo from './assets/tech_logo/jenkins.jpg';
+import confluenceLogo from './assets/tech_logo/confluence.jpg';
+import jestLogo from './assets/tech_logo/jest.png';
+
+// Experience Section Logo's
+import innowiseLogo from './assets/company_logo/innowise.png';
+import velseraLogo from './assets/company_logo/velsera.jpg';
+
+// Education Section Logo's
+import beLogo from './assets/education_logo/be.jpg';
+import cdacLogo from './assets/education_logo/sunbeam.jpg';
+
+// Project Section Logo's
+
+export const SOCIAL_LINKS = {
+  GIT: 'https://github.com/ravisolanke1407',
+  LINKEDIN: 'https://www.linkedin.com/in/ravindra-solanke-developer/',
+  FACEBOOK: 'https://www.facebook.com/ravisolanke1996',
+  INSTAGRAM: 'https://www.instagram.com/ravisolanke96/',
+  GMAIL: 'mailto:ravisolanke1407@gmail.com',
+  RESUME:
+    'https://drive.google.com/file/d/1PuOol78rffXhcGHWGwCwasC-y6Ii0i_H/view?usp=drive_link',
+};
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Chakra UI', logo: chakrauiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Nest JS', logo: nestjsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+    ],
+  },
+
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Figma', logo: figmaLogo },
+      { name: 'Parcel', logo: bundlerLogo },
+      { name: 'Webpack', logo: bundlerLogo },
+      { name: 'Vite', logo: bundlerLogo },
+      { name: 'ESLint & Prettier', logo: eslintPrettierLogo },
+    ],
+  },
+  {
+    title: 'Agile & Methodologies',
+    skills: [
+      { name: 'Jira', logo: jiraLogo },
+      { name: 'Confluence', logo: confluenceLogo },
+      { name: 'Agile Development (Scrum & Kanban)', logo: jiraLogo },
+      { name: 'Sprint Planning & Retrospectives with JIRA', logo: jiraLogo },
+      { name: 'Issue Tracking & Bug Triage', logo: jiraLogo },
+      { name: 'User Stories & Epics Management', logo: jiraLogo },
+      { name: 'Daily Stand-ups (Scrum rituals)', logo: jiraLogo },
+      { name: 'CI/CD Pipelines & Code Reviews', logo: jiraLogo },
+    ],
+  },
+  {
+    title: 'DevOps & Deployment',
+    skills: [
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Docker', logo: dockerLogo },
+      { name: 'Nginx', logo: nginxLogo },
+      { name: 'Jenkins', logo: jenkinsLogo },
+    ],
+  },
+
+  {
+    title: 'Others / Integrations',
+    skills: [
+      { name: 'Axios', logo: reactjsLogo },
+      { name: 'Redux / Zustand / Context API', logo: reactjsLogo },
+      { name: 'Rest API', logo: reactjsLogo },
+      { name: 'Graphql', logo: reactjsLogo },
+      { name: 'Mongoose', logo: reactjsLogo },
+      { name: 'Formik + Yup', logo: reactjsLogo },
+      { name: 'Socket.IO', logo: reactjsLogo },
+      { name: 'Razorpay', logo: reactjsLogo },
+    ],
+  },
+  {
+    title: 'Testing',
+    skills: [
+      { name: 'Jest', logo: jestLogo },
+      { name: 'React Testing Library', logo: reactjsLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: velseraLogo,
+    projectName: 'Clinical Genomics Workspace',
+    role: 'Senior Software Engineer',
+    company: 'PierianDx India Pvt Ltd (Velsera)',
+    date: 'April 2025 - Present',
+    desc: 'The Clinical Genomics Workspace (CGW) is a specialized web-based platform designed for physicians and clinical laboratory professionals in the fields of oncology and inherited diseases. The platform streamlines the process of establishing and managing Next-Generation Sequencing (NGS) testing programs by integrating all key components into a single, seamless workflow.',
+    skills: [
+      'ReactJS',
+      'Redux',
+      'JavaScript',
+      'Typescript',
+      'NestJS',
+      'Chakra UI',
+      'GraphQL',
+      'Rest API',
+      'TDD',
+      'React Testing Library',
+      'AWS',
+      'Docker',
+      'Kubernetes',
+      'CI/CD',
+      'Jenkins',
+      'Microservices',
+      'Agile Methodologies',
+    ],
+  },
+  {
+    id: 1,
+    img: velseraLogo,
+    projectName: 'Clinical Genomics Workspace',
+    role: 'Software Engineer',
+    company: 'PierianDx India Pvt Ltd (Velsera)',
+    date: 'January 2023 - April 2025',
+    desc: 'The Clinical Genomics Workspace (CGW) is a specialized web-based platform designed for physicians and clinical laboratory professionals in the fields of oncology and inherited diseases. The platform streamlines the process of establishing and managing Next-Generation Sequencing (NGS) testing programs by integrating all key components into a single, seamless workflow.',
+    skills: [
+      'ReactJS',
+      'Redux',
+      'JavaScript',
+      'Typescript',
+      'NestJS',
+      'Chakra UI',
+      'GraphQL',
+      'Rest API',
+      'TDD',
+      'React Testing Library',
+      'AWS',
+      'Docker',
+      'Kubernetes',
+      'CI/CD',
+      'Jenkins',
+      'Microservices',
+      'Agile Methodologies',
+    ],
+  },
+  {
+    id: 2,
+    img: innowiseLogo,
+    projectName: 'Virtual classrooms',
+    role: 'Senior Software Engineer',
+    company: 'Innowise LLC',
+    date: 'January 2021 - January 2023',
+    desc: 'A full-featured Virtual Classroom web application designed to facilitate real-time collaboration between students and instructors. The platform enables hands-on learning experiences using virtual machines, and offers features like public/private chat, voice calling, and interactive sessions, mimicking a real classroom environment.',
+    skills: ['React.js', 'Node.js', 'Material UI', 'Azure', 'Figma', 'Azure'],
+  },
+  {
+    id: 3,
+    img: innowiseLogo,
+    projectName: 'Virtual classrooms',
+    role: 'Software Engineer',
+    company: 'Innowise LLC',
+    date: 'July 2020 - January 2021',
+    desc: 'A full-featured Virtual Classroom web application designed to facilitate real-time collaboration between students and instructors. The platform enables hands-on learning experiences using virtual machines, and offers features like public/private chat, voice calling, and interactive sessions, mimicking a real classroom environment.',
+    skills: ['React.js', 'Node.js', 'Material UI', 'Azure', 'Figma', 'Azure'],
+  },
+  {
+    id: 4,
+    img: innowiseLogo,
+    projectName: 'Virtual classrooms',
+    role: 'Associate Software Engineer',
+    company: 'Innowise LLC',
+    date: 'March 2019 - July 2020',
+    desc: 'A full-featured Virtual Classroom web application designed to facilitate real-time collaboration between students and instructors. The platform enables hands-on learning experiences using virtual machines, and offers features like public/private chat, voice calling, and interactive sessions, mimicking a real classroom environment.',
+    skills: ['React.js', 'Node.js', 'Material UI', 'Azure', 'Figma'],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: cdacLogo,
+    school: 'Sunbeam Institute of Information Technology, Pune (CDAC)',
+    date: 'Feb 2019',
+    grade: '66%',
+    desc: 'Completed a rigorous, industry-focused training program in software development covering full-stack web technologies, data structures, OOPs, DBMS, operating systems, and software engineering. Built hands-on projects using Java, .NET, MERN stack, and gained exposure to tools like Git, Docker, and cloud deployment.',
+    degree: 'Post Graduate - CDAC (Diploma in Advanced Computing)',
+  },
+  {
+    id: 1,
+    img: beLogo,
+    school:
+      'Dr. Vitthalrao Vikhe Patil College of Engineering, Ahmednagar(Univ. of Pune)',
+    date: 'Jun 2017',
+    grade: '66%',
+    desc: 'Developed a strong foundation in electronics and communication systems, with growing interest in software development and IT. Explored programming, web technologies, and real-time systems through academic projects and self-learning.',
+    degree: 'BE (Electronics & Telecommunication)',
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: 'GitHub Profile Detective',
+    description:
+      'A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.',
+    image: '',
+    tags: ['HTML', 'CSS', 'JavaScript', 'React JS', 'API'],
+    github: '',
+    webapp: '',
+  },
+];
